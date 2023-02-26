5f820ebfabc664fdfec9ae5df9ef31ad45dc2653
Vladyslav Adianov <vladik.adianov@gmail.com>
2023-02-26 13:59:21 +0200

    add homework (normal)

--- Homework 31 - Cycles and Conditions/script.js
+++ Homework 31 - Cycles and Conditions/script.js
@@ -105,3 +105,110 @@
 //         console.log(i)
 //     }
 // }
+
+
+// task normal
+
+//Запитай у користувача п’ятирозрядне число і визначи, чи є воно паліндромом.
+
+// let userNumber = +prompt('Введіть п’ятирозрядне число:');
+// let userNumberReverse = 0;
+//
+// while (userNumber > userNumberReverse) {
+//     console.log(userNumber, userNumberReverse);
+//     userNumberReverse *= 10;
+//     userNumberReverse += userNumber % 10;
+//     userNumber = Math.trunc(userNumber / 10);
+// }
+//
+// console.log(userNumber, userNumberReverse);
+//
+// if (userNumber === userNumberReverse || userNumber === Math.trunc(userNumberReverse/10)) {
+//     console.log('Palindrome');
+// } else {
+//     console.log('Not a Palindrome');
+// }
+
+
+// Запитай у користувача суму покупки і виведи суму до оплати зі знижкою:
+// від 200 до 300 - знижка буде 3%;
+// від 300 до 500 - 5%;
+// від 500 і вище - 7%.
+
+// let userSum = +prompt('enter purchase amount:');
+// let userDiscount = 0;
+//
+// if (userSum >= 200 && userSum <= 299) {
+//     userDiscount = userSum * 3 / 100;
+// } else if (userSum >= 300 && userSum <= 499) {
+//     userDiscount = userSum * 5 / 100;
+// } else if (userSum >= 500) {
+//     userDiscount = userSum * 7 / 100;
+// }
+// console.log(`Your discount is ${userDiscount}$`);
+
+
+//Запитай у користувача 10 чисел і порахуй, скільки він ввів додатніх, від’ємних і нулів. При цьому також порахуй, скільки з них парних і непарних. Виведи статистику на екран.
+
+// let userNumbers;
+// let positiveNumbers = 0;
+// let negativeNumbers = 0;
+// let zerosNumbers = 0;
+// let evenNumbers = 0;
+// let oddNumbers = 0;
+//
+// for (let i = 0; i <= 9; i++) {
+//     userNumbers = +prompt('Введите число:');
+//     console.log('userNumbers', userNumbers);
+//     if (userNumbers < 0) {
+//         negativeNumbers++;
+//     }
+//     if (userNumbers > 0) {
+//         positiveNumbers++;
+//     }
+//     if (userNumbers % 2 === 0) {
+//         evenNumbers++;
+//     }
+//     if (userNumbers % 2 !== 0) {
+//         oddNumbers++;
+//     }
+//     if (userNumbers === 0) {
+//         zerosNumbers++;
+//     }
+// }
+//
+// console.log('positiveNumbers', positiveNumbers);
+// console.log('negativeNumbers', negativeNumbers);
+// console.log('zerosNumbers', zerosNumbers);
+// console.log('evenNumbers', evenNumbers);
+// console.log('oddNumbers', oddNumbers);
+
+
+//Зацикли відображення днів тижня таким чином: «День тижня. Хочеш побачити наступний день? » і так до тих пір, поки користувач натискає OK.
+
+// let dayOfTheWeek = confirm('Понеділок. Хочеш побачити наступний день?');
+//
+// if (dayOfTheWeek === true) {
+//     dayOfTheWeek = confirm('Вівторок. Хочеш побачити наступний день?');
+//
+//     if (dayOfTheWeek === true) {
+//         dayOfTheWeek = confirm('Середа. Хочеш побачити наступний день?');
+//
+//         if (dayOfTheWeek === true) {
+//             dayOfTheWeek = confirm('Четвер. Хочеш побачити наступний день?');
+//
+//             if (dayOfTheWeek === true) {
+//                 dayOfTheWeek = confirm("П'ятниця. Хочеш побачити наступний день?");
+//
+//                 if (dayOfTheWeek === true) {
+//                     dayOfTheWeek = confirm("Субота. Хочеш побачити наступний день?");
+//
+//                     if (dayOfTheWeek === true) {
+//                         dayOfTheWeek = confirm("Неділя. Хочеш побачити наступний день?");
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
